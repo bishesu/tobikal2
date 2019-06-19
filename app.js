609bd7ec62c796980e411340e7c86d0236721de0
bishes <bishesu@gmail.com>
2019-06-19 11:51:55 +0545

    Regestration successfully done through API

---
+++ app.js
@@ -0,0 +1,22 @@
+require('./database/connect');
+const User = require('./models/user');
+const multer = require('multer');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.post("/register", (req, res) => {
+    var myData = new User(req.body);
+    myData.save().then(function() {
+        res.send('User registered successfully');
+
+    }).catch(function(e) {
+        res.send(e)
+    });
+});
+
+app.listen(3000);
---
+++ models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const User = mongoose.model('user', {
+    firstname: {
+        type: String
+    },
+    lastname: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    dateofbirth: {
+        type: String
+    },
+    contact: {
+        type: String
+    },
+    username: {
+        type: String
+    },
+    password: {
+        type: String
+    }
+
+
+});
+
+module.exports = User;
